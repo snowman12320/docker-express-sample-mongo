3439188260a91ee416f80382bc100fd3fd180999
snowman12320 <snowman12320@gmail.com>
2025-03-21 10:14:10 +0800

    簡化 MongoDB 連接配置，移除不必要的選項

--- service/db.js
+++ service/db.js
@@ -3,10 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URL);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB 連接錯誤：'));
