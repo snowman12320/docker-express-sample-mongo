ef4ceda6967a924d22ed2d6e94c0c46f95c02064
william <101874444+snowman12320@users.noreply.github.com>
2025-05-14 09:44:45 +0000

    feat: develop on codesandbox

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const geminiRouter = require("./routes/geminiRouter");
 
-const indexRouter = require('./routes/index');
-const sleepDataRouter = require('./routes/sleepDataRouter');
-const pdfRouter = require('./routes/pdfRouter');
-const patientRouter = require('./routes/PatientRouter');
-const doctorRouter = require('./routes/DoctorRouter');
-const ctRouter = require('./routes/ctRouter');
+const indexRouter = require("./routes/index");
+// const sleepDataRouter = require('./routes/sleepDataRouter');
+// const pdfRouter = require('./routes/pdfRouter');
+// const patientRouter = require('./routes/PatientRouter');
+// const doctorRouter = require('./routes/DoctorRouter');
+// const ctRouter = require('./routes/ctRouter');
 
 const app = express();
 app.use(cors());
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/sleep', sleepDataRouter);
-app.use('/pdf', pdfRouter);
-app.use('/patients', patientRouter);
-app.use('/doctors', doctorRouter);
-app.use('/ct', ctRouter);
+app.use("/", indexRouter);
+// app.use('/sleep', sleepDataRouter);
+// app.use('/pdf', pdfRouter);
+// app.use('/patients', patientRouter);
+// app.use('/doctors', doctorRouter);
+// app.use('/ct', ctRouter);
+app.use("/gemini", geminiRouter);
 
 app.use(function (req, res, next) {
   next(createError(404));
@@ -37,9 +39,9 @@ app.use(function (req, res, next) {
 
 app.use(function (err, req, res, next) {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 const PORT = process.env.PORT || 3000;
